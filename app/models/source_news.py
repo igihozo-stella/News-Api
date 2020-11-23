9cc953cf91dd19788e30a661f3c4bb429ce1101d
igihozo-stella <igihozo96@gmail.com>
2020-11-23 11:02:06 +0200

    create source news class.

---
+++ app/models/source_news.py
@@ -0,0 +1,9 @@
+class News_Source:
+    
+    #newssource class to define news objects
+    
+    def __init__(self,id,name,url,category):
+        self.id = id
+        self.name = name
+        self.url = url
+        self.category = category
