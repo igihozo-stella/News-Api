4b6907b27fb9973ad90cd8c6ad1add514eacf766
igihozo-stella <igihozo96@gmail.com>
2020-11-23 11:16:28 +0200

    add sources url and article url in the gen config class.

---
+++ app/config.py
@@ -0,0 +1,28 @@
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SOURCE_NEWS_URL = 'https://newsapi.org/v2/sources?category={}&apiKey={}'
+    ARTICLE_NEWS_URL ='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
